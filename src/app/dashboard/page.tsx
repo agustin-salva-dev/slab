d4202b541445e23d0d3ee2fe4205a96ac0267054
sqlv <agvstin.77@gmail.com>
2026-02-18 18:37:26 -0300

    refactor: standardize font inheritance and interactive hover states

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,10 @@
 export default function Dashboard() {
     return (
         <div className="h-full py-10">
-            <div className="w-full flex gap-5 justify-center items-center text-sm font-power-med antialiased tracking-wider">
-                <p className="font-power-ultra cursor-pointer">My Slabs</p>
-                <p className="text-my-secondary cursor-pointer">Analytics</p>
-                <p className="text-my-secondary cursor-pointer">Settings</p>
+            <div className="group w-full flex gap-5 justify-center items-center font-power-ultra text-sm antialiased">
+                <p className="cursor-pointer">My Slabs</p>
+                <p className="text-my-secondary cursor-pointer t-color hover-white">Analytics</p>
+                <p className="text-my-secondary cursor-pointer t-color hover-white">Settings</p>
             </div>
         </div>
     )
