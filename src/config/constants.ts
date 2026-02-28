a896bea44afdb641298ca7281ac793797bdfbfa0
sqlv <agvstin.77@gmail.com>
2026-02-28 19:53:02 -0300

    chore: update latest feature link for navbar

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -6,7 +6,7 @@ export const SITE_LINKS = {
   linkedin: "https://www.linkedin.com/in/agustin-salva-dev/",
   xProfile: "https://x.com/agvsdev",
   latestFeature:
-    "https://github.com/agustin-salva-dev/slab/commit/eebe61075e5be3c9f162e36a0c1a2dcfa2bdad5a",
+    "https://github.com/agustin-salva-dev/slab/commit/49de45821b5ef157bbe0fae93b4de0cf5f11e78f",
   contributing:
     "https://github.com/agustin-salva-dev/slab/tree/master?tab=readme-ov-file#-getting-started",
 } as const;
