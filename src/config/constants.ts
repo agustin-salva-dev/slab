b077d97784ee2bf357d5c0c5e57364573069febb
Solu <agvstin.77@gmail.com>
2026-04-03 18:03:27 -0300

    chore(config): update latest feature link in navbar

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -6,7 +6,7 @@ export const SITE_LINKS = {
   linkedin: "https://www.linkedin.com/in/agustin-salva-dev/",
   xProfile: "https://x.com/agvsdev",
   latestFeature:
-    "https://github.com/agustin-salva-dev/slab/commit/3c01e9ff6c9f6704e638667ad6a271533218ae04",
+    "https://github.com/agustin-salva-dev/slab/commit/e0d5e2f4ce1c2070af7ba8a3ecbdf6bdd7e22fab",
   contributing:
     "https://github.com/agustin-salva-dev/slab/tree/master?tab=readme-ov-file#-getting-started",
   reportBug: "/report-bug",
